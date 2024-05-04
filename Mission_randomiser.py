8683bb86fcb83b62e3d99f2f8dcd4c2eeae86d66
Alexander <109859387+Vulpes1705@users.noreply.github.com>
2024-05-04 23:13:51 +0300

    Initial commit
    started a dict with twist options.

---
+++ Mission_randomiser.py
@@ -0,0 +1,8 @@
+import random 
+
+twists = {
+    "Calm weather" : "Nothing happens this time",
+    "Blood wind" : "Substract 1 from all melee hit rolls",
+    "Wild beasts" : "Some wild beasts appear!"
+}
+
